52ec71e12d808acdb942d640e5573a2434eb60d8
greczimarton <g.marci1999@gmail.com>
2023-12-02 23:40:02 +0100

    add solutions

--- day-02/src/bin/part2.rs
+++ day-02/src/bin/part2.rs
@@ -8,54 +8,42 @@ fn main() {
 
 fn process(input: &str) -> String {
     let lines: Vec<&str> = input.lines().collect();
-    let max_red: u32 = 12;
+
     let regex_red = Regex::new(r"\d+ red").unwrap();
-
-    let max_green: u32 = 13;
     let regex_green = Regex::new(r"\d+ green").unwrap();
-
-    let max_blue: u32 = 14;
     let regex_blue = Regex::new(r"\d+ blue").unwrap();
 
     let mut sum = 0;
 
     for line in lines {
         dbg!(line);
-        let too_many_red = is_color_too_many(line, &regex_red, &max_red);
-        if too_many_red {
+        let Some(min_num_of_red) = minimum_number_of_color(line, &regex_red) else {
             continue;
-        }
+        };
+        dbg!("red", min_num_of_red);
 
-        let too_many_green = is_color_too_many(line, &regex_green, &max_green);
-        if too_many_green {
+        let Some(min_num_of_green) = minimum_number_of_color(line, &regex_green) else {
             continue;
-        }
+        };
+        dbg!("green", min_num_of_green);
 
-        let too_many_blue = is_color_too_many(line, &regex_blue, &max_blue);
-        if too_many_blue {
+        let Some(min_num_of_blue) = minimum_number_of_color(line, &regex_blue) else {
             continue;
-        }
+        };
 
-        let game_id = line
-            .split(":")
-            .nth(0)
-            .expect("should have something like 'game 5' before ':'")
-            .split(" ")
-            .nth(1)
-            .expect("should have 2 elements game and id")
-            .to_string();
+        dbg!("blue", min_num_of_blue);
 
-        dbg!(&game_id);
+        let power = min_num_of_red * min_num_of_green * min_num_of_blue;
 
-        let game_id_num = game_id.parse::<u32>().expect("should parse id to u32");
-
-        sum += game_id_num;
+        sum += power;
     }
 
     sum.to_string()
 }
 
-fn is_color_too_many(line: &str, regex: &Regex, max_count: &u32) -> bool {
+fn minimum_number_of_color(line: &str, regex: &Regex) -> Option<u32> {
+    let mut max_count_of_color = 0;
+
     for regex_match in regex.find_iter(line) {
         let digit = regex_match
             .as_str()
@@ -67,12 +55,12 @@ fn is_color_too_many(line: &str, regex: &Regex, max_count: &u32) -> bool {
 
         let num = digit.parse::<u32>().expect("should parse to num");
 
-        if max_count < &num {
-            return true;
+        if max_count_of_color < num {
+            max_count_of_color = num;
         }
     }
 
-    return false;
+    return Some(max_count_of_color);
 }
 
 #[cfg(test)]
@@ -88,6 +76,6 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
         );
-        assert_eq!(result, "8");
+        assert_eq!(result, "2286");
     }
 }
